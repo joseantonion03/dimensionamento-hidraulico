fcd65c2c776cba7ab481b378732a62c44da56569
José Antônio <50558024+joseantonion03@users.noreply.github.com>
2023-09-29 09:04:43 -0300

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,39 @@
+<?php
+
+try {
+    $dados = json_decode(file_get_contents('php://input'));
+
+    if ($dados === null && json_last_error() !== JSON_ERROR_NONE) {
+        throw new Exception("Você precisa preecher as informações");
+    }
+
+    $calcTubulacao = floatval($dados->tubulacao);
+    $calcProfundidade = floatval($dados->profundidade);
+    $calcDistancia = floatval($dados->distancia);
+    $calcVazao = floatval($dados->vazao);
+    $calcAltura = floatval($dados->altura);
+
+    function calcularMCA($altura, $alturaReservatorio, $comprimento, $diametro, $vazaoDesejada, $material = 140)
+    {
+        $hf = (10.9611 * (pow((($vazaoDesejada / (1000 * 3600)) / $material), 1.852)) * $comprimento / (pow(($diametro / 1000), 4.8655)));
+
+        $mcaTotal = $hf + $altura + $alturaReservatorio;
+
+        return number_format($mcaTotal, 2, ',', '.');
+    }
+
+    if (
+        !empty($calcTubulacao)
+        && !empty($calcProfundidade)
+        && !empty($calcDistancia)
+        && !empty($calcVazao)
+        && !empty($calcAltura)
+    ) {
+        exit(json_encode(array('status' => true, 'result' => calcularMCA($calcProfundidade, $calcAltura, $calcDistancia, $calcTubulacao, $calcVazao))));
+    } else {
+        exit(json_encode(array('status' => false)));
+    }
+} catch (Exception $e) {
+    //throw $th;
+    exit(json_encode(array('message' => $e->getMessage())));
+}   
